3049c8f20aee146aa6f73c7e6027bb9a4f6c4e40
Kaiqi Liang <kaiqi.liang@outlook.com>
2024-01-30 09:27:49 +1100

    Change to async mutex

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 mod autotrader;
 mod book;
 mod feed;
@@ -27,7 +27,7 @@ async fn main() {
     println!("Server responded with headers: {:?}", response.headers());
     let (_, read) = stream.split();
 
-    let trader = Arc::new(Mutex::new(AutoTrader::new(
+    let trader = Arc::new(tokio::sync::Mutex::new(AutoTrader::new(
         Username::KLiang,
         String::from("de7d8b078d63d5d9ad4e9df2f542eca6"),
         Some(read),
@@ -35,38 +35,42 @@ async fn main() {
 
     trader
         .lock()
-        .unwrap()
+        .await
         .startup()
         .await
         .expect("Failed to connect to the feed and recover from the latest snapshot");
 
     println!(
         "Started up with books: {:#?}",
-        trader.lock().unwrap().books.keys(),
+        trader.lock().await.books.keys(),
     );
 
     let shutdown = trader.clone();
     tokio::spawn(async move {
         let _ = tokio::signal::ctrl_c().await;
-        shutdown.lock().unwrap().shutdown();
+        let _ = shutdown.lock().await.shutdown().await;
+        std::process::exit(1);
     });
 
-    let observations = Arc::new(Mutex::new(HashMap::new()));
+    let observations = Arc::new(std::sync::Mutex::new(HashMap::new()));
     loop {
         let observations_update = observations.clone();
         tokio::spawn(async move {
             let _ = refresh_latest_observations(observations_update).await;
         });
 
-        let _ = trader.lock().unwrap().poll().await;
-        for (product, book) in trader.lock().unwrap().books.iter() {
+        let trader_clone = trader.clone();
+        tokio::spawn(async move {
+            let _ = trader_clone.lock().await.poll().await;
+        });
+        for (product, book) in trader.lock().await.books.iter() {
             let credit = 10;
             let (best_bid, best_ask) = book.bbo();
             println!("{:?}", best_bid);
             println!("{:?}", best_ask);
             let _ = trader
                 .lock()
-                .unwrap()
+                .await
                 .place_order(
                     product,
                     best_bid.map_or(Price(1000), |price| price.price + credit),
@@ -77,7 +81,7 @@ async fn main() {
                 .await;
             let _ = trader
                 .lock()
-                .unwrap()
+                .await
                 .place_order(
                     product,
                     best_ask.map_or(Price(5000), |price| price.price - credit),
